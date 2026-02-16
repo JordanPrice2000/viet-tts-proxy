5868033e03fc7b54b4ab088e06ba096753785fbb
Jordan <jor.one@gmail.com>
2026-02-16 09:54:25 -0800

    Update tts.js

--- api/tts.js
+++ api/tts.js
@@ -1,6 +1,7 @@
 export default async function handler(req, res) {
   // Enable CORS
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  const origin = req.headers.origin || 'null';
+  res.setHeader('Access-Control-Allow-Origin', origin);
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   
